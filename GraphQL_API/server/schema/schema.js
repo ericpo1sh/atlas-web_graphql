08224f4c7f07058b3e5ffb450c84c941e342d438
ericpo1sh <ericpo1sh@gmail.com>
2024-07-05 12:01:00 -0500

    4. Type relations

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -4,23 +4,12 @@ const lodash = require('lodash');
 const TaskData = [
   {id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)', projectId: '1'},
   {id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order', projectId: '1'}
-]
+];
 
 const ProjectData = [
   {id: '1', title: 'Advanced HTML', weight: 1, description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!"},
   {id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'}
-]
-
-const TaskType = new GraphQLObjectType({
-  name: 'Task',
-  fields: () => ({
-    id: { type: GraphQLString },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-    description: { type: GraphQLString },
-    projectId: { type: GraphQLID },
-  })
-})
+];
 
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
@@ -32,11 +21,28 @@ const ProjectType = new GraphQLObjectType({
     tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return lodash.find(projects, { id: parent.projectId });
+        return lodash.filter(TaskData, { projectId: parent.id });
       }
     }
   })
-})
+});
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+    projectId: { type: GraphQLID },
+    project: {
+      type: ProjectType,
+      resolve(parent, args) {
+        return lodash.find(ProjectData, { id: parent.projectId });
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -50,7 +56,7 @@ const RootQuery = new GraphQLObjectType({
     },
     task: {
       type: TaskType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return lodash.find(TaskData, { id: args.id });
       },
